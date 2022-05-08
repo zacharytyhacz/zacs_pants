2b63d3a62d8a07e61dfb7b9ee161b4350a86e942
zacharytyhacz <zactyh@gmail.com>
2022-05-07 20:02:50 -0400

    Removed schedule date check and corrected type definitions

--- application/checkForPosts.ts
+++ application/checkForPosts.ts
@@ -1,7 +1,7 @@
 import posts from './posts.json'
 
 export type Post = {
-    datesToPost: number[]
+    datesToPost: string[]
     description: string
     hashtags: string[]
     mediaSource?: string
@@ -9,22 +9,6 @@ export type Post = {
     title: string
 }
 
-const socialMediaPlatforms = [
-    'linkedin'
-]
-
 export const checkForPosts = ():Post[] => {
-    let postsThatNeedToBePosted: Post[] = []
-    const now = Date.now()
-
-    posts.forEach((post) => {
-        socialMediaPlatforms.forEach((socialMediaPlatform) => {
-            const socialMediaPost: Post = post[socialMediaPlatform]
-            if (socialMediaPost.datesToPost.includes(now)) {
-                postsThatNeedToBePosted.push(socialMediaPost)
-            }
-        })
-    })
-
-    return postsThatNeedToBePosted
+    return posts
 }
