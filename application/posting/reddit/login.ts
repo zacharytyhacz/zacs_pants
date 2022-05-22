8274dcdee430ffae3d7a96f671a1a037f9a6d553
zacharytyhacz <zactyh@gmail.com>
2022-05-21 21:15:15 -0400

    😩💦 Got Reddit Login to work

--- application/posting/reddit/login.ts
+++ application/posting/reddit/login.ts
@@ -6,26 +6,20 @@ import { typeLikeHuman } from '../../human/type'
 export const logInToReddit = async (
     page: Page
 ): Promise<void> => {
-    await page.goto('https://www.reddit.com/')
+    await page.goto('https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F')
 
+    await pause(6.0) // bot is comtemplating life
+
+    const { username, password } = settings.reddit
+    await typeLikeHuman(page, '#loginUsername', username)
+
+    await pause(1.0)
+    await typeLikeHuman(page, '#loginPassword', password)
+
+    await pause(1.0)
+
+    await page.click('button[type=submit]')
+
+    // might change to pause amount
     await page.waitForNavigation()
-
-    const [loginButton] = await page.$x('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[1]/header/div/div[2]/div/div[1]/a[1]')
-    loginButton.focus()
-    await pause(2.0)
-    loginButton.click()
-
-    // await pause(2.0)
-
-    // const { username, password } = settings.reddit
-    // await typeLikeHuman(page, '#loginUsername', username)
-
-    // await pause(1.0)
-    // await typeLikeHuman(page, '#loginPassword', password)
-
-    // await pause(1.0)
-
-    // await page.click('button[type=submit]')
-
-    // await page.waitForNavigation()
 }
